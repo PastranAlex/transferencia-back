dee5888e82372473469a95bebe27df94ca5095dc
Alex Pastran <alex.pastran.b@gmail.com>
2021-04-09 00:48:38 -0400

    gitignore

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@
 const app = require('./server')
 require('dotenv').config()
 const { dbConnection } = require('../src/database/config')
-const PORT = process.env.PORT;
 
 app.get('/', (req,res) => res.send('API ejercicio ripley'));
 app.listen(process.env.PORT, async () => {
   await dbConnection();
-  console.log(`server corriendo en https://localhost:${PORT}`);
+  console.log(`server corriendo en https://localhost:${process.env.PORT}`);
 });
 
